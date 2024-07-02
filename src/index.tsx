08d2ca883698804edf74b09ad8c3ef369689cb8f
James Abaja <jgabaja@gmail.com>
2024-07-03 01:03:24 +0800

    Update createRoot implementation

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const domNode = document.getElementById("root");
+if (domNode) {
+  const root = createRoot(domNode);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
